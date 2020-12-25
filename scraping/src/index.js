91151fc7876bda350beb9151e905baf5164760ae
Hasegawa0313 <heritakopoochan@gmail.com>
2020-12-25 18:39:23 +0900

    全ページのリンク取得のロジック作成

--- scraping/src/index.js
+++ scraping/src/index.js
@@ -3,29 +3,65 @@ const puppeteer = require("puppeteer");
 async function transitionTest(url, noTestPage) {
   const browser = await puppeteer.launch({
     args: ["--disable-web-security", "--no-sandbox", "--disable-setuid-sandbox"],
-    headless: false,
   });
 
-  this.options = { waitUntil: "domcontentloaded", timeout: 0 };
-
-  await page.goto(url, this.options);
-
-  const linkList = await page.evaluate(() => {
-    const nodeList = document.querySelectorAll(`a`);
-    const hrefList = Array.from(nodeList).map((html) => {
-      return html.href;
-    });
-    return hrefList;
-  });
-
-  console.log(linkList);
+  const options = { waitUntil: "domcontentloaded", timeout: 0 };
 
   const page = await browser.newPage();
+
+  let linkList = [url];
+  let allLinkList = [];
+  let listExists = true;
+  while (listExists) {
+    let resultList = [];
+    let resultLinkList = [];
+    for (pageUrl of linkList) {
+      await page.goto(pageUrl, options);
+
+      // ページのaタグ取得
+      const pageLinkList = await page.evaluate((pageUrl) => {
+        const nodeList = document.querySelectorAll(`a`);
+        const hrefList = Array.from(nodeList).map((html) => {
+          return html.href;
+        });
+        return { pageUrl, hrefList };
+      }, pageUrl);
+      if (
+        allLinkList.length <= 0 ||
+        (!allLinkList.some((link) => link.pageUrl === pageLinkList.pageUrl) &&
+          (resultList.length <= 0 || !resultList.some((result) => pageLinkList.pageUrl === result.pageUrl)))
+      ) {
+        resultList.push(pageLinkList);
+
+        // すでに取得済みのリンク以外を取り出す
+        let resultHrefList = pageLinkList.hrefList;
+        resultHrefList.forEach((resultlink) => {
+          if (
+            (allLinkList.length <= 0 || !allLinkList.some((link) => link.pageUrl === resultlink)) &&
+            (resultList.length <= 0 || !resultList.some((result) => resultlink === result.pageUrl))
+          ) {
+            resultLinkList.push(resultlink);
+          }
+        });
+      }
+    }
+
+    // 未取得分を付け足す
+    allLinkList = allLinkList.concat(resultList);
+    if (resultLinkList && resultLinkList.length <= 0) {
+      listExists = false;
+    } else {
+      linkList = resultLinkList;
+    }
+  }
+
+  console.log(allLinkList);
+
   await browser.close();
 }
 
-async () => {
-  const url = "";
+(async () => {
+  const url = "http://localhost:3111";
   const noTestPage = null;
   await transitionTest(url, noTestPage);
-};
+})();
--- test_sample/routes/users.js
+++ test_sample/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  // res.send("respond with a resource");
+  res.render("users", "");
+});
+
+router.get("/:userId", function (req, res, next) {
+  // res.send("respond with a resource");
+  res.render(`user`, req.params);
 });
 
 module.exports = router;
